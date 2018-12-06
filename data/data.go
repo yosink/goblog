0a706b05a9546e69fd6a64c2f89d231f812bfe86
yuxinke <yuxinke@ecmoho.com>
2018-12-06 18:17:06 +0800

    update time format

--- data/data.go
+++ data/data.go
@@ -10,8 +10,9 @@ import (
 )
 
 const (
-	connectStr      = "root:root@(127.0.0.1)/default?charset=utf8&parseTime=True&loc=Local"
-	TIME_FORMAT_STR = "2006-01-02 15:04:05"
+	connectStr       = "root:root@(127.0.0.1)/default?charset=utf8&parseTime=True&loc=Local"
+	TIME_FORMAT_TIME = "2006-01-02 15:04:05"
+	TIME_FORMAT_DATE = "2006-01-02"
 )
 
 var (
@@ -22,14 +23,14 @@ type LocalTime time.Time
 
 // MarshalJSON 重写格式化为json的方法
 func (l LocalTime) MarshalJSON() ([]byte, error) {
-	stamp := fmt.Sprintf("\"%s\"", time.Time(l).Format("2006-01-02 15:04:05"))
+	stamp := fmt.Sprintf("\"%s\"", time.Time(l).Format(TIME_FORMAT_TIME))
 	return []byte(stamp), nil
 }
 
 type LocalDate time.Time
 
 func (l LocalDate) MarshalJSON() ([]byte, error) {
-	stamp := fmt.Sprintf("\"%s\"", time.Time(l).Format("2006-01-02"))
+	stamp := fmt.Sprintf("\"%s\"", time.Time(l).Format(TIME_FORMAT_DATE))
 	return []byte(stamp), nil
 }
 
