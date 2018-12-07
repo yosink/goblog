f7a78d1bb42125c3e54f80bae09a26b86aef147a
yuxinke <yuxinke@ecmoho.com>
2018-12-07 18:09:22 +0800

    add http server

--- main.go
+++ main.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 	"goblog/data"
 	"log"
+	"net/http"
 )
 
 func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("./templates"))
+	mux.Handle("/static/", http.StripPrefix("/static", files))
+	mux.HandleFunc("/", index)
+
+	s := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+	log.Fatal(s.ListenAndServe())
 	art, err := data.GetArticleByID(1)
 	if err != nil {
 		log.Fatal(err)
